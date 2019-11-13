778ea3087bfa643741967ba0e341607c17a8dc8f
Grace Petrosini <gpetrosi@nd.edu>
2019-11-13 16:25:43 -0500

    #2 of Part 2 done.

--- Part2.R
+++ Part2.R
@@ -1,16 +1,16 @@
 iris<-read.csv("./Biocomp_exercise07/iris.csv")
 
 #number of observations for a given species
-observations<-function(df, subSet, species){
-  spec<-df$subSet
-  dat<-length(which(spec==species))
-  
+spec<- iris$Species
+observations<-function(subSet, species_name){
+  list(a = df, b=sub, c=species_name)
+  dat<-length(subSet[subSet == "c"])
   return(dat)
 }
 
-observations(iris, Species, virginica)
+observations(spec, virginica)
 
-#flowers with Sepal.Width>given number
+       #flowers with Sepal.Width>given number
 sepalWidth<-function(df, num){
   width<-df[which(df[,2]>num),]
   return(width)
@@ -19,8 +19,12 @@ sepalWidth(iris, 3.0)
 
 
 #data for a given species to [species].csv file
-speciesData<-function(species_name){
-  
+speciesData<-function(df, sub, species_name){
+  for(n in df$sub){
+    
+  }
+  name<-paste("./Biocomp_exercise07/",species_name, ".csv")
+  write.csv(dat, file = name)
 }
 
 
