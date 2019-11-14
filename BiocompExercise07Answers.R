685707605b6251423f6f6271a39bbb8ed73ce5e2
Mary Solokas <msolokas@nd.edu>
2019-11-14 17:40:50 -0500

    done

--- BiocompExercise07Answers.R
+++ BiocompExercise07Answers.R
@@ -1,5 +1,5 @@
 # Part 1- return the odd rows of any dataframe
-file <- read.csv(file="~/Desktop/Biocomp/Biocomp_exercise06/wages.csv")
+wages <- read.csv(file="~/Desktop/Biocomp/Biocomp_exercise06/wages.csv") # used wages to test function
 
 oddRows <- function(file){
   NumbRows=nrow(file)
@@ -7,24 +7,27 @@ oddRows <- function(file){
   oddRows = file[oddNumbers,]
   return(oddRows)
 }
-oddRows(file)
+oddRows(file=wages)
 
 # Part 2
 iris=read.csv("~/Desktop/Biocomp/Biocomp_exercise06/iris.csv")
 
-NumbSequence <-function(iris, species){
-  givenSpecies = iris[iris$Species=="species"]
+# To return the number of observations of a given species in a data set
+NumbSequence <-function(fileName, species){
+  givenSpecies <- fileName[fileName$Species==species,] #establishes rule that data must have column title Species
   output = nrow(givenSpecies)
   return(output)
 }
-NumbSequence(iris, setosa)
-# Output should be 50 for each one
+NumbSequence(iris, "setosa") #Returns number of setosas
+NumbSequence(iris, "versicolor") #Returns number of veriscolors
+NumbSequence(iris, "virginica") #Returns number of virginicas
 
 #To find the individuals with a sepal width greater than x
 greater_than_width<-function(x){
   width<-iris[iris$Sepal.Width>x,]
   return(width)
 }
+greater_than_width(3.5) #Returns dataframe of individuals with sepal width greater than 3.5
 
 #To create a .csv file of a specified species
 speciesFile<-function(iris, speciesName){
