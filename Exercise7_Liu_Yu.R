23a1b3efcb8c515d81003dc712cfed06d05b7e6d
katnip <katnip@katnipdembp.dhcp.nd.edu>
2019-11-15 02:07:12 -0500

    gave another solution for question 1 and finished the question 2

--- Exercise7_Liu_Yu.R
+++ Exercise7_Liu_Yu.R
@@ -10,3 +10,41 @@ oddrows = function(df){
   }
   print(df[row.index,])
 }
+
+# Alternative solution to Question 1
+oddrows = function(df){
+  rownumber = nrow(df)
+  row.index = c()
+  for (i in c(1:rownumber)){
+    if(i %% 2 == 1){
+      row.index = c(row.index,i)
+    }
+  }
+  print(df[row.index,])
+}
+
+
+# Question 2
+# load dataset
+data = read.csv('iris.csv')
+
+# return the number of observations for a given species included in the data set
+number.of.obs = function(species){
+  data.species = data[which(data$Species==species),]
+  number.obs = nrow(data.species)
+  print(number.obs)
+}
+
+# return a dataframe for flowers with Sepal.Width greater than a value specified by the function user
+df.of.spl = function(n){
+  data.spl = data[which(data$Sepal.Width>n),]
+  data.spl
+}
+
+# write the data for a given species to a comma-delimited file with the given species name as the file name; Hint: look at paste() to add the .csv extension to your file in the for-loop.
+write.species = function(species){
+  data.species = data[which(data$Species==species),]
+  write.csv(data.species,paste(species,'.csv',sep = ''))
+}
+
+
